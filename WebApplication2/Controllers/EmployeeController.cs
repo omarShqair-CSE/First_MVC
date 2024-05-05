3dfe6493437264d26c1e1bdd569821eb3597759e
Omar AbuShqair <omarshqair854@gmail.com>
2024-05-06 02:16:15 +0300

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ApplicationDbContext context=new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var employee= context.Employees1.ToList();
+            return View("Index",employee);
+        }
+    }
+}
---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees1 { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ABU-SHQAIR\\SQLEXPRESS;Database=FS8_MVC1;trusted_Connection=True;TrustServerCertificate=true;");
+        }
+    }
+}
